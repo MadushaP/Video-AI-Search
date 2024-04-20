bf9d6965e07b9ac80aa00c0e0ade4552d2386f6d
Madusha Pallawela <madusha@hotmail.co.uk>
2024-04-20 10:57:07 +0100

    Added route and button link to video level analysis

--- video-ai-search/src/components/ProfileCard.tsx
+++ video-ai-search/src/components/ProfileCard.tsx
@@ -4,8 +4,9 @@ import YouTube, { YouTubeProps } from 'react-youtube';
 // Import the profile interface from data.js
 import { iProfile } from "../services/data";
 import { useEffect, useRef, useState } from 'react';
+import Link from 'next/link';
 
-function YouTubePlayer({ playerRef, isPlaying, videoID }: { playerRef: React.MutableRefObject<any>, isPlaying: boolean, videoID:string }) {
+function YouTubePlayer({ playerRef, isPlaying, videoID }: { playerRef: React.MutableRefObject<any>, isPlaying: boolean, videoID: string }) {
   const onPlayerReady: YouTubeProps['onReady'] = (event) => {
     playerRef.current = event.target;
   };
@@ -42,14 +43,21 @@ export const ProfileCard = (props: iProfile) => {
     }
   }, [playerRef, jumpButtonId]);
 
-  const { email, role, videoID } = props;
-//sort out data strcuure
+  const { role, videoID } = props;
+  //sort out data strcuure
   return (
-    <div className="profile__card rounded-[15px] border border-solid" style={{ width: '540px' }}>
-      <button id={jumpButtonId}>Jump to 30 seconds</button>
+    <div className="profile__card rounded-[15px] border border-solid" style={{ width: '542px' }}>
       <YouTubePlayer playerRef={playerRef} isPlaying={isPlaying} videoID={videoID} />
-      <div className=" bg-slate-300 p-3">
-        <h2 className="">VideoID: {videoID}</h2>
+      <div className="info-container d-flex flex-column justify-content-end align-items-end bg-slate-300 p-3" style={{ height: '163px' }}>
+        <h2 className="pb-4">VideoID: {videoID}
+          <div className="float-right">
+            <Link href={`/video/[videoid]`} as={`/video/${videoID}`}>
+              <button className="bg-blue-100 hover:bg-blue-500 rounded-md p-2 text-blue-700">
+                More Info
+              </button>
+            </Link>
+          </div>
+        </h2>
         <p>Labels: {role}</p>
       </div>
     </div>
